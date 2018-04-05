187280777f4fd20489a068779a5b4f030e1f4e11
Tong <zwstcoy@gmail.com>
2018-04-05 17:56:37 -0400

    Add files via upload

--- PageRank.py
+++ PageRank.py
@@ -7,45 +7,45 @@ sc = SparkContext(conf = conf)
 sc.setLogLevel("ERROR")
 
 # Load the adjacency list file
-AdjList1 = sc.textFile("/home/tong/data/02AdjacencyList.txt")
-print AdjList1.collect()
+AdjList1 = sc.textFile("--/02AdjacencyList.txt")
+print(AdjList1.collect())
 
-AdjList2 = AdjList1.map(lambda line : line.split(" "))  # 1. Replace the lambda function with yours
-print "AdjList2"
-AdjList3 = AdjList2.map(lambda x:(int(x[0]),[int(y) for y in x[1:]]))  # 2. Replace the lambda function with yours
+#read the file and split by space
+AdjList2 = AdjList1.map(lambda line : line.split(" "))
+
+AdjList3 = AdjList2.map(lambda x:(int(x[0]),[int(y) for y in x[1:]]))
+
+#save in memory
 AdjList3.persist()
-print AdjList3.collect()
 
+# count the total node in file
 nNumOfNodes = AdjList3.count()
-print "Total Number of nodes"
-print nNumOfNodes
 
-# Initialize each page's rank; since we use mapValues, the resulting RDD will have the same partitioner as links
-print "Initialization"
-PageRankValues = AdjList3.mapValues(lambda v : .2)  # 3. Replace the lambda function with yours
-print PageRankValues.collect()
-print "PageRank"
-# Run 10 iterations of PageRank
-print "Run 30 Iterations"
+# Initialize each page's rank with .2
+PageRankValues = AdjList3.mapValues(lambda v : .2)
+
+# Run 30 iterations of PageRank
+print ("Run 30 Iterations")
 for i in range(1, 31):
-    print "Number of Iterations"
-    print i
+    print("Number of Iterations","\n",i)
+
+    #combin the node list with pagerank value
     JoinRDD = AdjList3.join(PageRankValues)
-    print "join results"
-    print JoinRDD.collect()
+
+    #calculate the degree for each node
     contributions = JoinRDD.flatMap(lambda (x, (y, z)) : [(v,z/len(y)) for v in y])
-    print "contributions"
-    print contributions.collect()
-    accumulations = contributions.reduceByKey(lambda x, y : x+y)  # 5. Replace the lambda function with yours
-    print "accumulations"
-    print accumulations.collect()
 
-    PageRankValues = accumulations.mapValues(lambda v : v*.85+.15/float(nNumOfNodes))  # 6. Replace the lambda function with yours
-    print "PageRankValues"
-    print PageRankValues.collect()
+    #combin the value of each node
+    accumulations = contributions.reduceByKey(lambda x, y : x+y)
 
-print "=== Final PageRankValues ==="
-print PageRankValues.collect()
+    #calcuate the final pagerank value with c=.85
+    PageRankValues = accumulations.mapValues(lambda v : v*.85+.15/float(nNumOfNodes))
 
-# Write out the final ranks
-PageRankValues.coalesce(1).saveAsTextFile("/home/tong/data/PageRankValues_Final")
+    print("PageRankValues")
+    print(PageRankValues.collect())
+
+print ("=== Final PageRankValues ===")
+print (PageRankValues.collect())
+
+# save the final result
+PageRankValues.coalesce(1).saveAsTextFile("--/PageRankValues_Final")
